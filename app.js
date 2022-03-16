b936623cb645a59190e4595c45659cce3113660e
Jason Hunter <jahunter767@gmail.com>
2022-03-16 17:27:46 -0500

    fixed button listeners to match the new pages

--- app.js
+++ app.js
@@ -21,31 +21,10 @@ window.onload = async function () {
         console.log("Home");
     }); // End-homeBtn listener
 
-    let page1Btn = document.getElementById("page-1-btn");
-    page1Btn.addEventListener("click", async function(event) {
+    let projectsBtn = document.getElementById("projects-btn");
+    projectsBtn.addEventListener("click", async function(event) {
         event.preventDefault();
-        await loadPage("./detailed-page-template.html");
-        console.log("Page 1");
-    }); // End-page1Btn listener
-
-    let page2Btn = document.getElementById("page-2-btn");
-    page2Btn.addEventListener("click", async function(event) {
-        event.preventDefault();
-        await loadPage("./detailed-page-template.html");
-        console.log("Page 2");
-    }); // End-page2Btn listener
-
-    let page3Btn = document.getElementById("page-3-btn");
-    page3Btn.addEventListener("click", async function(event) {
-        event.preventDefault();
-        await loadPage("./detailed-page-template.html");
-        console.log("Page 3");
-    }); // End-page3Btn listener
-
-    let page4Btn = document.getElementById("page-4-btn");
-    page4Btn.addEventListener("click", async function(event) {
-        event.preventDefault();
-        await loadPage("./detailed-page-template.html");
-        console.log("Page 4");
-    }); // End-page4Btn listener
+        await loadPage("./projects-page.html");
+        console.log("Projects Page");
+    }); // End-projectsBtn listener
 }; // End-window.onload
